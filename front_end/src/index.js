54cb735f5a984956675d7ab0eb8ee278a55d8e69
Fdika24 <71744284+Fdika24@users.noreply.github.com>
2021-08-08 14:48:30 +0700

    Router added

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AppRouter from './Router/Router'
+import App from './Views/Home/App'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppRouter />
   </React.StrictMode>,
   document.getElementById('root')
 );
