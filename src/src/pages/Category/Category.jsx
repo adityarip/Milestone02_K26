2082fabe002cd9be424e4fcc6b34425a6d307844
Aditya <73730555+adityarip@users.noreply.github.com>
2021-08-08 21:32:57 +0800

    pages dan components
    penambahan pages dan componentsnya

---
+++ src/src/pages/Category/Category.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Category = () => {
+    return (
+        <div>
+            <h1>Ini kategori</h1>
+            <h1>Ini kategori</h1>
+            <h1>Ini kategori</h1>
+            <h1>Ini kategori</h1>
+            <h1>Ini kategori</h1>
+        </div>
+    )
+}
+
+export default Category
