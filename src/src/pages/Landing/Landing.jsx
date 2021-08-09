5c743814310a211d3151b3afc03db45744275644
Fdika24 <71744284+Fdika24@users.noreply.github.com>
2021-08-09 15:42:41 +0700

    Tailwind css added

--- src/src/pages/Landing/Landing.jsx
+++ src/src/pages/Landing/Landing.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-
+import Navbar from '../../NavBar/NavBar'
 const Landing = () => {
     return (
-        <div>
-            <h1>test</h1>
-        </div>
+        <Navbar></Navbar>
     )
 }
 
