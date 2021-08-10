8700f3ba1aabca5c89fc0fb294257afb3c860b60
Noxira <42240815+Noxira@users.noreply.github.com>
2021-08-10 21:03:07 +0700

    v1.0 Get Started buttons
    get started sama sudah siap!?!?!? v1 jelek amat af

--- src/src/pages/Landing/Landing.jsx
+++ src/src/pages/Landing/Landing.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Weeklycard from '../../components/LandingComponents/Weeklycard'
+import Getstarted from '../../components/LandingComponents/Getstarted'
 
 const Landing = () => {
     return (
         <div>
             <Weeklycard/>
             <h1>test</h1>
+            <Getstarted/>
         </div>
     )
 }
