e59c9d4ea1f825bf42a3a020df9d428487d17b61
Jeremy <73146752+JeremyRio@users.noreply.github.com>
2021-08-13 15:37:48 +0700

    v1.0 Footer

--- src/src/pages/Landing/Landing.jsx
+++ src/src/pages/Landing/Landing.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Weeklycard from '../../components/LandingComponents/Weeklycard'
 import Getstarted from '../../components/LandingComponents/Getstarted'
-
+import BottomFooter from '../../components/LandingComponents/BottomFooter'
 const Landing = () => {
     return (
         <div>
-            <Weeklycard/>
+            <Weeklycard />
             <h1>test</h1>
-            <Getstarted/>
+            <Getstarted />
+            <BottomFooter />
         </div>
     )
 }
