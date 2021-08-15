a069f3b61e2aa9a62744b80314e6681af1d32c56
Aditya <73730555+adityarip@users.noreply.github.com>
2021-08-15 19:47:40 +0800

    Revert "Full-app"
    This reverts commit 2850d5912912c4ace2ef9e7896573f75d966c39a.

--- src/src/routes/routes.js
+++ src/src/routes/routes.js
@@ -1,7 +1,5 @@
 // Import Component
 import Landing from "../pages/Landing/Landing"
-import Weekly from "../pages/Weekly/Weekly"
-import Category from "../pages/Category/Category"
 
 // buat return {label,path,component} yang bakal di ambil di App.js
 const route = (label, path, component) => ({
@@ -12,8 +10,6 @@ const route = (label, path, component) => ({
 
 // Bikin route
 export const LANDING_PAGE = route('Landing', '/', Landing)
-export const WEEKLY_PAGE = route('Weekly Top', '/weekly', Weekly)
-export const CATEGORY_PAGE = route('Category', '/category', Category)
 //Misal nanti ada page ini:
 
 // yang gak bisa diakses tanpa authentication
@@ -23,7 +19,5 @@ export const AllPrivateRoutes = [
 
 export const AllOpenRoutes = [
     LANDING_PAGE,
-    WEEKLY_PAGE,
-    CATEGORY_PAGE,
 ]
 
