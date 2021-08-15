97cdcc17aab5ff202e804bfb9eda20534eebfe89
Fdika24 <71744284+Fdika24@users.noreply.github.com>
2021-08-15 21:06:02 +0700

    Navbar

--- src/src/NavBar/NavBar.js
+++ src/src/NavBar/NavBar.js
@@ -1,15 +1,36 @@
 import React from 'react'
 import './NavBar.css'
+
 const Navbar = () => {
-    return (
-        <div className="border-2 border-black border-dashed bg-blue-500">
-            <ul>
-                <li><a href="\">Home</a></li>
-                <li><a href="\">About</a></li>
-                <li><a href="\">Login</a></li>
-            </ul>
-        </div>
-    )
-}
+  let rutes = [
+    {
+      rute: "one",
+      name: "weekly's top",
+    },
+    {
+      rute: "two",
+      name: "Home",
+    },
+    {
+      rute: "three",
+      name: "Category & Review",
+    },
+  ];
+  return (
+    <nav class="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between pl-9 py-4 px-6 bg-transparent sm:items-baseline w-full">
+      <div class="mb-2 sm:mb-0">
+        <a href="/" class="text-2xl no-underline text-grey-darkest hover:text-blue-dark">Home</a>
+      </div>
+      <div>
+        {rutes.map((rute) => {
+          return (
+            <a href={`/${rute.rute}`} class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">{rute.name}</a>
+          );
+        })}
+        <button class="bg-yellow-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full">Login</button>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar
