82db842bff18ad456787b9ed111a571d8c43974f
Aditya <73730555+adityarip@users.noreply.github.com>
2021-08-17 00:13:02 +0800

    Update Category.jsx

--- src/src/pages/Category/Category.jsx
+++ src/src/pages/Category/Category.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import HeaderCat from '../../components/CategoryComponents/HeaderCat'
 import BottomFooter from '../../components/LandingComponents/BottomFooter'
 import RekomCat from '../../components/CategoryComponents/RekomCat'
+import Navbar from '../../NavBar/NavBar'
 
 const Category = () => {
     return (
         <div>
+            <Navbar/>
             <HeaderCat/>
             <RekomCat/>
             <BottomFooter/>
