a8a62f6b35216d913ebafa4618190c572f48069b
Amelia Wahyu <ameliawahyu2@gmail.com>
2021-08-16 22:06:29 +0700

    update category

--- src/src/pages/Category/Category.jsx
+++ src/src/pages/Category/Category.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HeaderCat from '../../components/CategoryComponents/HeaderCat'
 import BottomFooter from '../../components/LandingComponents/BottomFooter'
+import RekomCat from '../../components/CategoryComponents/RekomCat'
 
 const Category = () => {
     return (
         <div>
             <HeaderCat/>
+            <RekomCat/>
             <BottomFooter/>
         </div>
     )
