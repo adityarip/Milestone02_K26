1a45b658d6dc98fd67ff68124d47394c80593656
Aditya <73730555+adityarip@users.noreply.github.com>
2021-08-16 18:25:26 +0800

    Update category page
    tambah navbar

--- src/src/pages/Category/Category.jsx
+++ src/src/pages/Category/Category.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeaderCat from '../../components/CategoryComponents/HeaderCat'
 import BottomFooter from '../../components/LandingComponents/BottomFooter'
+import Navbar from '../../NavBar/NavBar'
 
 const Category = () => {
     return (
         <div>
+            <Navbar/>
             <HeaderCat/>
             <BottomFooter/>
         </div>
